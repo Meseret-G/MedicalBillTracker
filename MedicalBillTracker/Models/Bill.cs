5e4d937bbbd8597ca8052187f04e713226de75c3
Meseret-G <negash4@gmail.com>
2022-05-21 22:30:40 -0500

    Models added

---
+++ MedicalBillTracker/Models/Bill.cs
@@ -0,0 +1,14 @@
+﻿namespace MedicalBillTracker.Models
+{
+    public class Bill
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Provider { get; set; }
+        public DateOnly ServiceDate { get; set; }
+        public string? ImageURL { get; set; }
+        public decimal OutOfPocket { get; set; }
+        public bool? IsOpen { get; set; }
+        public int PatientId { get; set; }
+    }
+}
