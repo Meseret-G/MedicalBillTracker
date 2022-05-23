ff68fd25d3118456bf3a881834444f798b1bac08
Meseret-G <negash4@gmail.com>
2022-05-23 18:30:28 -0500

    Able to get Bill By ID.

--- MedicalBillTracker/Repos/BillRepo.cs
+++ MedicalBillTracker/Repos/BillRepo.cs
@@ -52,5 +52,41 @@ namespace MedicalBillTracker.Repos
             }
         }
 
+        public Bill? GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id,Title, [Provider],ImageURL,OutOfPocket, IsOpen
+                        FROM Bill
+                        WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Bill bill = new Bill()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Provider = reader.GetString(reader.GetOrdinal("Provider")),
+                                //ServiceDate = reader.GetDateTime(reader.GetOrdinal("ServiceDate")),
+                                ImageURL = reader.GetString(reader.GetOrdinal("ImageURL")),
+                                OutOfPocket = reader.GetDecimal(reader.GetOrdinal("OutofPocket")),
+                                IsOpen = reader.GetBoolean(reader.GetOrdinal("IsOpen"))
+                            };
+                            return bill;
+                        }
+                        else return null;
+                    }
+                }
+            }
+        }
+
+
     }
 }
