74ca46a6ed6d20fba0fc27c2c89dd130d9b55a52
Meseret-G <negash4@gmail.com>
2022-05-24 17:47:31 -0500

    Able to get patient by email.

--- MedicalBillTracker/Controllers/PatientController.cs
+++ MedicalBillTracker/Controllers/PatientController.cs
@@ -31,75 +31,18 @@ namespace MedicalBillTracker.Controllers
                 return Ok(newPatient);
             }
         }
+        // GET api/<PatientController>/Email/5
+        [HttpGet("Email/{email}")]
+        public IActionResult GetPatientByEmail(string email)
+        {
+            Patient patient = _patientRepo.GetPatientByEmail(email);
 
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return Ok(patient);
+        }
 
-        //// GET: PatientController/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    return View();
-        //}
-
-        //// GET: PatientController/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: PatientController/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create(IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: PatientController/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: PatientController/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: PatientController/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: PatientController/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
     }
 }
