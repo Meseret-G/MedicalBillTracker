7dc8ab10de30b48b5a186e4dd6f4226781a42fea
Meseret-G <negash4@gmail.com>
2022-05-26 19:43:41 -0500

    minor updates

--- medicalbilltracker-client/src/Data/auth/apiKey.js
+++ medicalbilltracker-client/src/Data/auth/apiKey.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from '@firebase/app';
+import { getAuth } from '@firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FB_API_KEY,
-  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FB_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_FB_ID,
-  measurementId: process.env.REACT_FB_APP_MEASUREMENT_ID,
+  apiKey: process.env.local.REACT_APP_API_KEY,
+  authDomain: process.env.local.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.local.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.local.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.local.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.local.REACT_APP_APP_ID,
+  measurementId: process.env.local.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
